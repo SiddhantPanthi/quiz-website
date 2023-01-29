e8169d14e10c96aef4b5305d2b700df65657d57b
Siddhant Panthi <iconic68.siddhant@gmail.com>
2023-01-29 23:19:30 +0545

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,48 @@
+let questions = [{
+        numb: 1,
+        question: "Which was the first detective satellite sent by USA on 18th august 1960",
+        answer: "Corona",
+        options: [
+            "Sputnik",
+            "Appolo 11",
+            "ISS",
+            "Corona"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the full form of JSON?",
+        answer: "Java Script Object Notation",
+        options: [
+            "B.S.2003 Ashoj 04",
+            "B.S.2002 Ashoj 01",
+            "B.S.2004 Ashoj 01",
+            "B.S.2002 Ashoj 04"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the full form of DOM?",
+        answer: "Document Object Model",
+        options: [
+            "Document Oriented Model",
+            "Document Object Model",
+            "Document Object Malware",
+            "Document Orientation Module"
+        ]
+    },
+    {
+        numb: 4,
+        question: "When did the war of American Independence take place?",
+        answer: "1776 A.D",
+        options: [
+            "1776 A.D",
+            "1774 A.D",
+            "1772 A.D",
+            "1770 A.D"
+        ]
+    },
+    
+    
+
+];
